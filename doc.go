346b85ec9286109ddd42f8759872603719a4da38
Leigh McCulloch <leigh@mcchouse.com>
2021-01-08 22:38:14 -0800

    Create doc.go

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Define your main function so that it matches the signature of testcli.MainFunc.
+// In tests, call your main function using testcli.Main which will capture stdout,
+// stderr, and the exit code. Use the other helper functions to create a working
+// directory that suites the use cases that need testing.
+//
+// For an example of this used in the wild, see the tests of gas: https://4d63.com/gas
+package testcli
