acd4a343ecbde6b3c14d04c34f2240bcc3bf2830
Leigh McCulloch <leigh@mcchouse.com>
2021-05-28 06:41:24 +0000

    Move funcs to package

--- testcli.go
+++ testcli.go
@@ -9,20 +9,10 @@ import (
 	"testing"
 )
 
-func New(tb testing.TB) Helper {
-	return Helper{
-		TB: tb,
-	}
-}
-
-type Helper struct {
-	TB testing.TB
-}
-
 type MainFunc func(args []string, stdin io.Reader, stdout, stderr io.Writer) int
 
-func (h Helper) Main(args []string, stdin io.Reader, main MainFunc) (exitCode int, stdout, stderr string) {
-	h.TB.Helper()
+func Main(t testing.TB, args []string, stdin io.Reader, main MainFunc) (exitCode int, stdout, stderr string) {
+	t.Helper()
 	combinedBuilder := strings.Builder{}
 	stdoutBuilder := strings.Builder{}
 	stderrBuilder := strings.Builder{}
@@ -32,15 +22,14 @@ func (h Helper) Main(args []string, stdin io.Reader, main MainFunc) (exitCode in
 		io.MultiWriter(&stdoutBuilder, &combinedBuilder),
 		io.MultiWriter(&stderrBuilder, &combinedBuilder),
 	)
-	h.TB.Log(strings.Join(args, " ") + "\n" + strings.TrimSuffix(string(combinedBuilder.String()), "\n"))
+	t.Log(strings.Join(args, " ") + "\n" + strings.TrimSuffix(string(combinedBuilder.String()), "\n"))
 	stdout = stdoutBuilder.String()
 	stderr = stderrBuilder.String()
 	return
-
 }
 
-func (h Helper) Exec(command string) (exitCode int, stdout, stderr string) {
-	h.TB.Helper()
+func Exec(t testing.TB, command string) (exitCode int, stdout, stderr string) {
+	t.Helper()
 	combinedBuilder := strings.Builder{}
 	stdoutBuilder := strings.Builder{}
 	stderrBuilder := strings.Builder{}
@@ -51,51 +40,51 @@ func (h Helper) Exec(command string) (exitCode int, stdout, stderr string) {
 	if err != nil {
 		if err, ok := err.(*exec.ExitError); ok {
 		} else {
-			h.TB.Fatalf("executing %q: %v", command, err)
+			t.Fatalf("executing %q: %v", command, err)
 		}
 	}
-	h.TB.Log(command + "\n" + strings.TrimSuffix(string(combinedBuilder.String()), "\n"))
+	t.Log(command + "\n" + strings.TrimSuffix(string(combinedBuilder.String()), "\n"))
 	exitCode = cmd.ProcessState.ExitCode()
 	stdout = stdoutBuilder.String()
 	stderr = stderrBuilder.String()
 	return
 }
 
-func (h Helper) Chdir(dir string) {
-	h.TB.Helper()
+func Chdir(t testing.TB, dir string) {
+	t.Helper()
 
 	err := os.Chdir(dir)
 	if err != nil {
-		h.TB.Fatalf("chdir to %q: %v", dir, err)
+		t.Fatalf("chdir to %q: %v", dir, err)
 	}
-	h.TB.Log("cd " + dir)
+	t.Log("cd " + dir)
 }
 
-func (h Helper) MkdirTemp() string {
-	h.TB.Helper()
+func MkdirTemp(t testing.TB) string {
+	t.Helper()
 
 	dir, err := os.MkdirTemp("", "")
 	if err != nil {
-		h.TB.Fatalf("mkdirtemp: %v", err)
+		t.Fatalf("mkdirtemp: %v", err)
 	}
-	h.TB.Log("mktemp -d\n" + dir)
+	t.Log("mktemp -d\n" + dir)
 	return dir
 }
 
-func (h Helper) Mkdir(path string) {
-	h.TB.Helper()
+func Mkdir(t testing.TB, path string) {
+	t.Helper()
 
 	err := os.MkdirAll(path, 0755)
 	if err != nil {
-		h.TB.Fatalf("mkdir %q: %v", path, err)
+		t.Fatalf("mkdir %q: %v", path, err)
 	}
 }
 
-func (h Helper) WriteFile(filename string, data []byte) {
-	h.TB.Helper()
+func WriteFile(t testing.TB, filename string, data []byte) {
+	t.Helper()
 
 	err := ioutil.WriteFile(filename, []byte{}, 0644)
 	if err != nil {
-		h.TB.Fatalf("writing file to %q: %v", filename, err)
+		t.Fatalf("writing file to %q: %v", filename, err)
 	}
 }
